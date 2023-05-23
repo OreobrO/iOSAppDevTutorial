4c583e8fb9f030a929b03956806f6efbe1bcf5cf
henry.choi <oreobro@naver.com>
2023-05-23 16:30:27 +0900

    First Commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 최민규 on 2023/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
